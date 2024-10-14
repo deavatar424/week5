51a77aa7ae5feb5671dbfa9252284bed458933c1
deavatar <deavatar4@gmail.com>
2024-10-14 14:55:43 +0100

    assigmentweek5

--- author.js
+++ author.js
@@ -1,8 +1,49 @@
-{
-    "book_id": "UUID",            // Unique identifier for the book
-    "title": "string",            // Title of the book
-    "publication_date": "date",   // Date when the book was published
-    "genre": "string",            // Genre of the book
-    "author_id": "UUID"           // Foreign key referencing the Author
-  }
-  
+const mongoose = require('mongoose');
+
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: true
+    },
+    birthDate: {
+        type: Date,
+        required: true
+    },
+    nationality: {
+        type: String,
+        required: true
+    },
+});
+
+// Book Schema
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    publicationDate: {
+        type: Date,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Author',  // Reference to the Author model
+        required: true
+    },
+});
+
+// Create models
+const Author = mongoose.model('Author', authorSchema);
+const Book = mongoose.model('Book', bookSchema);
+
+// Export models
+module.exports = { Author, Book };
